d58154a76c892de18577773a02492edc1c627f79
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-10-07 21:26:53 -0300

    Delete main/kotlin directory

--- main/kotlin/jogocartas/Carta.kt
+++
@@ -1,9 +0,0 @@
-package jogocartas
-
-data class Carta(
-    val nome: String,
-    val descricao: String,
-    val ataque: Int,
-    val defesa: Int,
-    val tipo: String
-)
--- main/kotlin/jogocartas/Monstro.kt
+++
@@ -1,8 +0,0 @@
-package jogocartas
-
-data class Monstro(
-    val nome: String,
-    val ataque: Int,
-    val defesa: Int,
-    var estado: String
-)
